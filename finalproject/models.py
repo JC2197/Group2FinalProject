86e468a69802e1fab048d79ff22969de8dc7eb03
Emilio <istemilioespada@gmai.com>
2023-11-30 17:37:18 -0500

    Further progress on CRUD implementation.

--- finalproject/models.py
+++ finalproject/models.py
@@ -10,12 +10,12 @@ class UserProfile(models.Model):
 
 
 class SavedEvents(models.Model):
-    name = models.CharField(max_length=64, default='')
-    image = models.CharField(max_length=64, default='')
-    date = models.CharField(max_length=64, default='')
-    time = models.CharField(max_length=64, default='')
-    venue = models.CharField(max_length=64, default='')
-    city = models.CharField(max_length=64, default='')
-    state = models.CharField(max_length=64, default='')
-    address = models.CharField(max_length=64, default='')
-    link = models.CharField(max_length=128, default='')
+    name = models.CharField(max_length=64)
+    image = models.CharField(max_length=64)
+    date = models.CharField(max_length=64)
+    time = models.CharField(max_length=64)
+    venue = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    state = models.CharField(max_length=64)
+    address = models.CharField(max_length=64)
+    link = models.CharField(max_length=128)
